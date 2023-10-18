7a92571d3624b6d4f6e41fad7a6e1d245009cddb
DhruvPatel0223 <77132882+DhruvPatel0223@users.noreply.github.com>
2023-10-17 22:45:24 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,14 +48,14 @@ function setup(){
 
 function draw(){
     console.log(bird.body.speed);
-    if(backgroundImg){
-        background(backgroundImg);
+    
+        background(bg);
     
         noStroke();
         textSize(35)
         fill("white")
         text("Score  " + score, width-300, 50)
-    }
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
