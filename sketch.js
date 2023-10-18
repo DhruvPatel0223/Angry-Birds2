db2fb121e6b23c4109a02382ddb11fcc1a3fe82c
DhruvPatel0223 <77132882+DhruvPatel0223@users.noreply.github.com>
2023-10-17 22:29:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -109,5 +109,14 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
     bg = "bg1.png";
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
